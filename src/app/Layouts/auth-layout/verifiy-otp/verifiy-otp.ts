db57696f18901bbbb56efc27dfe1610a02aa3358
Ahmed Aboelsoud <aboelsoudahmed345@gmail.com>
2026-03-07 03:15:24 +0200

    update routes

--- src/app/Layouts/auth-layout/verifiy-otp/verifiy-otp.ts
+++ src/app/Layouts/auth-layout/verifiy-otp/verifiy-otp.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { InputOtpModule } from 'primeng/inputotp';
 
 @Component({
   selector: 'app-verifiy-otp',
-  imports: [InputOtpModule,FormsModule],
+  imports: [InputOtpModule,FormsModule,RouterLink],
   templateUrl: './verifiy-otp.html',
   styleUrl: './verifiy-otp.css',
 })
